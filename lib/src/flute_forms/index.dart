fc8f84beef3eccbad8d302985d5de41af3cbcbfa
ragokan <okanyldrmop@gmail.com>
2021-10-24 20:36:19 +0300

    yes

--- lib/src/flute_forms/index.dart
+++ lib/src/flute_forms/index.dart
@@ -1,4 +1,4 @@
 export './flute_form_provider.dart';
-export './flute_form_builder.dart';
-export './flute_form_field.dart';
+export 'widgets/flute_form_builder.dart';
+export 'widgets/flute_form_field.dart';
 export './flute_form_model.dart';
