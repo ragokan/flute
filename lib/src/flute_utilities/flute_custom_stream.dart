8d6483d13f95677dcf1afd6168ffa6b6c43130f6
ragokan <okanyldrmop@gmail.com>
2021-10-24 10:52:24 +0300

    fcs

---
+++ lib/src/flute_utilities/flute_custom_stream.dart
@@ -0,0 +1,28 @@
+typedef _TCallback<T> = void Function(T value);
+
+class FluteCustomStream<T> {
+  final List<_TCallback> _listeners = [];
+
+  /// Notifies all listeners.
+  void notifyListeners(T value) {
+    for (var listener in _listeners) {
+      try {
+        listener(value);
+      } catch (error) {
+        print('Error happened while calling the listener: $listener - $error');
+      }
+    }
+  }
+
+  void listen(_TCallback listener) {
+    _listeners.add(listener);
+  }
+
+  void removeListener(_TCallback listener) {
+    _listeners.remove(listener);
+  }
+
+  void dispose() {
+    _listeners.clear();
+  }
+}
