aaf1e0bc721b03117a51528a6d8a406f484d7149
Your Name <you@example.com>
2021-11-23 21:50:26 +0300

    yz

---
+++ lib/src/utilities/list_functions.dart
@@ -0,0 +1,30 @@
+List<T> insert<T>(List<T> list, T item) => [item, ...list];
+
+List<T> add<T>(List<T> list, T item) => [...list, item];
+
+List<T> where<T>(List<T> list, bool Function(T item) test) => [
+      for (var item in list)
+        if (test(item)) item
+    ];
+
+List<T> addAndFilter<T>(List<T> list, item, bool Function(T item) test) => [
+      item,
+      for (var item in list)
+        if (test(item)) item
+    ];
+
+List<T> insertAndFilter<T>(List<T> list, item, bool Function(T item) test) => [
+      for (var item in list)
+        if (test(item)) item,
+      item
+    ];
+
+T? firstWhereOrNull<T>(List<T> list, bool Function(T item) test) {
+  T? result;
+  for (var item in list) {
+    if (test(item)) {
+      return result = item;
+    }
+  }
+  return result;
+}
