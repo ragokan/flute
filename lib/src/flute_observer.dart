6e4bd58d7594e048b840cd351dcb8cba6d6ce0f0
Your Name <you@example.com>
2021-11-25 19:04:33 +0300

    yz

--- lib/src/flute_observer.dart
+++ lib/src/flute_observer.dart
@@ -1,17 +1,19 @@
 import 'package:flute/flute.dart';
 
 abstract class FluteObserver {
+  const FluteObserver();
+
   static FluteObserver? observer;
 
   void onNotifierError<State>(
     FluteNotifier<State> notifier, {
     required Object error,
     StackTrace? stackTrace,
-  }) {}
+  });
 
   void onStorageError(
     String type, {
     required Object error,
     StackTrace? stackTrace,
-  }) {}
+  });
 }
